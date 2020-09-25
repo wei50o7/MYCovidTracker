c5cbdaf2d0250af95dde418038519a21abe21dca
wei50o7 <61506795+wei50o7@users.noreply.github.com>
2020-09-25 19:41:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="js/main.js"></script>
+  </head>
+
+  <body>
+    <h1 class="title">COVID-19 Tracker</h1>
+    <h2 class="subtitle">The latest statistic regarding the "Corona Virus".</h2>
+    <div class="data-container">
+      <div class="stats-container">
+        <h4>Cases</h4>
+        <h1 id="cases"></h1>
+        <h4>Deaths</h4>
+        <h1 id="deaths"></h1>
+        <h4>Percentage of deaths</h4>
+        <h1 id="percentage"></h1>
+      </div>
+      <div class="location-container">
+        <h4>Country</h4>
+        <h1 id="country">Malaysia</h1>
+        <h4>Population</h4>
+        <h1 id="population"></h1>
+        <h4>Last Updated</h4>
+        <h1 id="update"></h1>
+      </div>
+    </div>
+    <h1 class="footer">Information provided by <a href="https://systems.jhu.edu/research/public-health/ncov">Johns Hopkins University</a></h1>
+  </body>
+
+</html>
